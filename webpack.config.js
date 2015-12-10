a100960f8634b85ffe15903c525f71daf4aac28e
Dominic Lind <linddominic@gmail.com>
2015-12-10 07:39:15 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,29 @@
+var webpack = require('webpack');
+
 module.exports = {
     entry: "./entry.js",
     output: {
         path: __dirname,
         filename: "dist/bundle.js"
     },
+    plugins : [
+      new webpack.ProvidePlugin({
+        riot : 'riot'
+      }),
+    ],
     module: {
+      preLoaders: [
+    			{
+            test: /\.tag$/, exclude: /node_modules/, loader: 'riotjs-loader', query: { type: 'none' }
+          }
+    		],
         loaders: [
 					{
 		        test: /\.scss$/,
 		        loader:"style-loader!css-loader!autoprefixer-loader?browsers=last 2 versions!sass-loader"
 		      },
 					{
-						test: /\.js?$/,
+						test: /\.js|\.tag$/,
 						exclude: /(node_modules|bower_components)/,
 						loader: 'babel?presets[]=es2015'
 					},
